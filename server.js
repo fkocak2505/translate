258c35e72fad0291c462eb70314c1a6313a22da9
Fatih Koçak <fkocak2505@gmail.com>
2019-10-24 19:44:57 +0300

    update
    update

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express")
+var app = express()
+
+const API_VERSION = "0.0.1"
+
+app.get('/api/public', (req, res) => {
+    res.json({ message: "Hello Server" })
+})
+
+var port = process.env.PORT || 3010
+app.listen(port, function() {
+    console.log("Server listening on: ", port)
+})
